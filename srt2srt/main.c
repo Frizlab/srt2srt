f24e5656dfa15ed63754979d7256b070440238ce
Frizlab <fload@me.com>
2011-04-24 16:09:37 +0200

    Initial Commit

---
+++ srt2srt/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  srt2srt
+//
+//  Created by François LAMBOLEY on 4/24/11.
+//  Copyright 2011 Frost Land. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+	// insert code here...
+	printf("Hello, World!\n");
+    return 0;
+}
+
