4d3a2a97de33734b17285805e1fbc805d285e5e1
Frizlab <fload@me.com>
2012-06-20 21:58:46 +0200

    Trailing whitespaces removed.

--- srt2srt/constants.h
+++ srt2srt/constants.h
@@ -22,8 +22,8 @@ typedef enum e_error {
 typedef struct s_srt2srt_options {
 	BOOL verbose;       /* Default: NO */
 	
-	const char *input;  /* No default, must be set. */	
-	const char *output; /* Default: NULL. Means stdout. */	
+	const char *input;  /* No default, must be set. */
+	const char *output; /* Default: NULL. Means stdout. */
 	
 	int delay_before;   /* Default: 0 */
 	int delay_after;    /* Default: 0 */
